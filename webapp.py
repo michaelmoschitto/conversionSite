9828acf0ec2eac7bb74ee2a9825fe667141d86e4
michaelmoschitto <mikemoschitto@gmail.com>
2017-10-10 09:29:28 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -4,17 +4,14 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 
 @app.route("/")
 def render_main():
-    return render_template('home.html')
+    return render_template('menu.html')
 
 @app.route("/response")
 def render_response():
-    color = request.args['color'] #the request object stores the information about the request sent to the server.
+    mass = float(request.args['mass']) #the request object stores the information about the request sent to the server.
             #the args field is a multi dict which is like a dict except it can have multiple values for the same key.
             # the inforation in args ifs visible in the url for the page being requested (ex... /response?color=blue)
-    if color == 'pink':
         reply = "Thats my favorite color too!"
-    else:
-        reply = "My favorite color is pink."
     return render_template('response.html', response = reply)
     
     
