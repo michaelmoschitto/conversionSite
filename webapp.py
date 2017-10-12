5be1b2316ebb3ce58d583c19271a4128e3cf3473
Michael Moschitto <michaelmoschitto@gmail.com>
2017-10-12 09:34:03 -0700

    render

--- webapp.py
+++ webapp.py
@@ -6,8 +6,8 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 def render_main():
     return render_template('menu.html')
 
-@app.route("/response")
-def render_response():
+@app.route("/kilograms")
+def render_kilograms():
     mass = float(request.args['mass']) #the request object stores the information about the request sent to the server.
             #the args field is a multi dict which is like a dict except it can have multiple values for the same key.
             # the inforation in args ifs visible in the url for the page being requested (ex... /response?color=blue)
