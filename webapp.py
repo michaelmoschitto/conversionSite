3402d69fec9d2f9ca5fdaf2bb72ee83ff1b2813b
Michael Moschitto <michaelmoschitto@gmail.com>
2017-10-12 09:58:52 -0700

    ^^

--- webapp.py
+++ webapp.py
@@ -8,10 +8,11 @@ def render_main():
 
 @app.route("/kilograms")
 def render_kilograms():
-    # mass = float(request.args['mass']) #the request object stores the information about the request sent to the server.
+    if mass in float(request.args['mass']):
+            #the request object stores the information about the request sent to the server.
             #the args field is a multi dict which is like a dict except it can have multiple values for the same key.
             # the inforation in args ifs visible in the url for the page being requested (ex... /response?color=blue)
-    reply = "Thats my favorite color too!"
+        reply = mass
     return render_template('kilograms.html', response = reply)
 
 
