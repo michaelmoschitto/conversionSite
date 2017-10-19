6025f28a92d19f916cf0db07efb4984ddb163976
michaelmoschitto <mikemoschitto@gmail.com>
2017-10-18 23:02:57 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -3,8 +3,8 @@ from flask import Flask, url_for, render_template, request
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
 @app.route("/")
-def render_home():
-    return render_template('home.html')
+def render_conversionPage():
+    return render_template('conversionPage.html')
 
 @app.route("/responseK")
 def render_responseK():
